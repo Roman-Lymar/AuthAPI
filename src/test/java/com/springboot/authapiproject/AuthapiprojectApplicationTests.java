3c7f3b2de891341951839d3118680f633e1ce77c
Trofymov Pavel <paveltrof86@gmail.com>
2021-03-31 18:48:03 +0300

    Start project

---
+++ src/test/java/com/springboot/authapiproject/AuthapiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.authapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthapiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
